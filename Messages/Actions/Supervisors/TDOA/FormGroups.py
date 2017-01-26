c517cfa5575f95aea492088cfc3f5e0638d300cf
Myshj <donmarin@mail.ru>
2017-01-26 12:48:26 +0200

    Added SensorSupervisorToTDOAGroupSupervisorConnector.

--- Messages/Actions/Supervisors/TDOA/FormGroups.py
+++ Messages/Actions/Supervisors/TDOA/FormGroups.py
@@ -7,11 +7,11 @@ class FormGroups(Base):
     Сообщение о том, что нужно сформировать группы из датчиков.
     """
 
-    def __init__(self, sender: Actor, sensors: list):
+    def __init__(self, sender: Actor, sensors: set):
         """
         Конструктор.
         :param Actor sender: Адресант сообщения.
-        :param list sensors: Датчики, из которых нужно сформировать группы.
+        :param set sensors: Датчики, из которых нужно сформировать группы.
         """
         super().__init__(sender)
         self.sensors = sensors
